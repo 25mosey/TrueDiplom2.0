d4c6fd3db7507e426b83f3061d89fba98fa3ee91
Fremy <dp8446732@gmail.com>
2024-06-11 22:25:00 +0200

    Contract

---
+++ Diplom/Controllers/ContractController.cs
@@ -0,0 +1,36 @@
+﻿using Diplom.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diplom.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContractController : ControllerBase
+    {
+        private DiplomContext db;
+        public ContractController(DiplomContext ctx)
+        {
+            db = ctx;
+        }
+
+        [HttpPost]
+        public void SaveContract([FromBody] Contract contract)
+        {
+            if (contract != null)
+            {
+                db.Contracts.Add(contract);
+                db.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IEnumerable<Contract> GetContracts()
+        {
+            return db.Contracts.ToList();
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Contract> GetContracts(int id)
+        {
+            return db.Contracts.Where(p => p.PartnerId == id).ToList();
+        }
+    }
+}
